9d9baface001f3ff496d65f749476541765ff1a8
lmiletic <l.miletic@msgnetconomy.net>
2019-06-11 16:21:57 +0200

    Practice

---
+++ exercise.ts
@@ -0,0 +1,16 @@
+// let bankAccount = {
+//     money: 2000,
+//     deposit(value) {
+//         this.money += value;
+//     }
+// };
+
+// let myself = {
+//     name: "Luka",
+//     bankAccount: bankAccount,
+//     hobbies: [ "Gaming", "Sports" ]
+// };
+
+// myself.bankAccount.deposit(3000);
+
+// console.log(myself);
