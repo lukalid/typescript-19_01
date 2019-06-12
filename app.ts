d8f57e249350d5761a2235e8f0b687120e35eb90
lmiletic <l.miletic@msgnetconomy.net>
2019-06-12 15:14:06 +0200

    Decorators

--- app.ts
+++ app.ts
@@ -1,32 +1,103 @@
-class MyMap<T> {
+function logged(constructorFn: Function) {
+    console.log(constructorFn);
+}
 
-    private map: { [key: string]: T } = {};
+@logged
+class Person {
+    constructor() {
+        console.log("Hi!");
+    }
+}
 
-    setItem(key: string, item: T) {
-        this.map[key] = item;
+// Factory
+function logging(value: boolean): any {
+    return value ? logged : null;
+}
+
+@logging(true)
+class Car {
+
+}
+
+// Advanced
+function printable(constructorFn: Function) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    }
+}
+
+@logging(true)
+@printable
+class Plant {
+    name = "Green Plant";
+}
+const plant = new Plant();
+(<any>plant).print();
+
+// Method Decorator
+// Property Decorator
+function editable(value: boolean) {
+    return function (target: any, propName: string, descriptor: PropertyDescriptor) {
+        descriptor.writable = value;
+    };
+}
+
+function overwritable(value: boolean) {
+    return function (target: any, propName: string): any {
+        const newDescriptor: PropertyDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+
+class Project {
+
+    @overwritable(true)
+    projectName: string;
+
+    constructor(name: string) {
+        this.projectName = name;
     }
 
-    getItem(key: string) {
-        return this.map[key];
+    @editable(false)
+    calcBudget() {
+        console.log(1000);
+    }
+}
+
+const project = new Project("Super Project");
+project.calcBudget();
+// project.calcBudget = function () {
+//     console.log(2000);
+// }
+project.calcBudget();
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+    console.log("target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);
+}
+
+class Course {
+
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
     }
 
-    clear() {
-        this.map = {};
-    }
-
-    print() {
-        console.log("Map:");
-        for (let key in this.map) {
-            console.log(key + " -> " + this.map[key]);
+    printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+        if (printAll) {
+            console.log(1000);
+        } else {
+            console.log(500);
         }
     }
 
 }
 
-const numberMap = new MyMap<number>();
-numberMap.setItem("apples", 10);
-numberMap.setItem("bananas", 2);
-console.log(numberMap.getItem("apples"));
-numberMap.print();
-numberMap.clear();
-numberMap.print();
+const course = new Course("Lame course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
