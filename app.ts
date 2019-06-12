056954cb2116ee7ffc9ffe0a3e97c4395e9ef4a2
lmiletic <l.miletic@msgnetconomy.net>
2019-06-12 13:17:38 +0200

    Interfaces

--- app.ts
+++ app.ts
@@ -1,7 +1,82 @@
-// import { PI, calculateCircumference } from "./math/circle";
-import * as Circle from "./math/circle"; // leave out .ts   typescript auto looks for it
-import calc from "./math/rectangle";
+interface NamedPerson {
+    name: string;
+    sport?: string;
+    [propName: string]: any;
+}
 
-console.log(Circle.PI);
-console.log(Circle.calculateCircumference(3));
-console.log(calc(10, 20));
+function greet(person: NamedPerson): void { // at least have a name
+    console.log("Hello, " + person.name);
+}
+
+function changeName(person: NamedPerson, newName: string): void {
+    person.name = newName;
+}
+
+const person: NamedPerson = {
+    name: "Luka",
+    age: 23 // -> binds to propName
+};
+
+greet(person);
+changeName(person, "Anna");
+greet(person);
+
+greet({ name: "Luka" }); // yep
+//greet({ name: "Luka", age: 23 }); // nope
+
+greet({ name: "Luka" }); // yep
+greet({ name: "Luka", sport: "Gaming" }); // yep
+
+// --------------------------------------------------
+
+interface Operation {
+    execute(num1: number, num2: number): number;
+}
+
+const adder: Operation = {
+    execute(number1: number, number2: number): number {
+        return number1 + number2;
+    }
+};
+console.log(adder.execute(1, 2));
+
+class Add implements Operation {
+    execute(number1: number, number2: number): number {
+        return number1 + number2;
+    }
+}
+class Multiply implements Operation {
+    execute(number1: number, number2: number): number {
+        return number1 * number2;
+    }
+}
+
+let operation = new Add();
+console.log(operation.execute(1, 2));
+operation = new Multiply();
+console.log(operation.execute(1, 2));
+
+// Function Types
+// Whatever uses this interface, must be a function of this type
+interface DoubleValueFunc {
+    (number1: number, number2: number): number;
+}
+
+let myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function (number1: number, number2: number) {
+    return (number1 + number2) * 2;
+}
+
+console.log(myDoubleFunction(1, 2));
+
+// Interface Inheritance
+interface AgedPerson extends NamedPerson {
+    age: number;
+}
+
+const oldPerson: AgedPerson = {
+    age: 60,
+    name: "Luka"
+}
+
+console.log(oldPerson);
--- math/rectangle.ts
+++
@@ -1,3 +0,0 @@
-export default function calculateRectangle(width: number, length: number): number {
-    return width * length;
-}
