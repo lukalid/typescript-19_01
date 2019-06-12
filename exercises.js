d75a3f2eb926ac7b6ea8f68b80638d5076ca2075
lmiletic <l.miletic@msgnetconomy.net>
2019-06-12 10:05:48 +0200

    ES6 Practise

---
+++ exercises.js
@@ -0,0 +1,34 @@
+// // Exercise 1
+// var double = function (value) {
+//     return value * 2;
+// }
+// console.log(double(10));
+
+// // Exercise 2
+// var hello = function (name) {
+//     if (name === undefined) { name = "Max"; }
+//     console.log("Hello, " + name);
+// }
+// hello();
+// hello("Anna");
+
+// // Exercise 3
+// var numbers = [1, 2, 3, 4, 5];
+// console.log(Math.min.apply(Math, numbers));
+
+// // Exercise 4
+// var newArray = [1, 2];
+// Array.prototype.push.apply(newArray, numbers);
+// console.log(newArray);
+
+// // Exercise 5
+// var testResults = [1, 2, 3];
+// var result1 = testResults[0];
+// var result2 = testResults[1];
+// var result3 = testResults[2];
+
+// // Exercise 6
+// var scientist = {firstName: "Will", experience: 12};
+// var firstName = scientist.firstName;
+// var experience = scientist.experience;
+// console.log(firstName, experience);
